d42f5d68685844b0680b019cbd45ce1f5b19b9d3
silvadiogo9 <dass_9@hotmail.com>
2023-04-18 18:34:13 +0100

    Starting User Inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,13 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
-import React from "react";
+import React, {useState} from "react";
 
 function ExpenseItem(props) {
-    let title = props.title;
+    const [title, setTitle] = useState(props.title);    
 
     const clickHandler = () => {
-        title = 'Updated';
+        setTitle('Updated');
     };
 
     return (
